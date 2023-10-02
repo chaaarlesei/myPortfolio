37cf4c6a08a8226f3413bb72d911d9998e5f7cc6
zzyzzy30 <perez301mizzy@gmail.com>
2023-10-03 00:27:07 +0800

    swiper wip

--- assets/script/main.js
+++ assets/script/main.js
@@ -88,3 +88,34 @@ modalCloses.forEach((modalClose) => {
         })
     })
 })
+
+// PORTFOLIO
+// const swiper = new Swiper('.portfolio__container', {
+
+//     loop: true,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//   });
+
+  const swiper = new Swiper('.portfolio__container', {
+    loop: true,
+    navigation: {
+        // nextEl: '.swiper-button-next',
+        // prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    clickable: true,
+    keyboard: true,
+})
